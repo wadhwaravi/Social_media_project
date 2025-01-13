9ea993ba1880a1a571b1ee7d1bc4844a92b0c777
wadhwaravi <117942469+wadhwaravi@users.noreply.github.com>
2025-01-13 19:56:16 +0530

    improved login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -74,7 +74,7 @@ export const login = async (req, res) => {
       return res.status(400).json({ message: "User not found" });
     }
 
-    const isMatch = bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
